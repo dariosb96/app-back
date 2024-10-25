fc6c14577cd4de6be434f06c13071c1339408c9c
Dario Sosa <xdsb.tstx@gmail.com>
2024-10-25 14:17:19 -0600

    last 1.1.1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT
+
 conn.sync({ force: true }).then(() => {
     server.listen(3000, () => {
-      console.log('%s listening at 3000'); // eslint-disable-line no-console
+      console.log(`listening at ${port}`); // eslint-disable-line no-console
+      // console.log(`listening at 3000`); // eslint-disable-line no-console
     });
   });
 
