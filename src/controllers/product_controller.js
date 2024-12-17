aa6b5e1315ab74bea7c0dad522429e34fcc7dc23
Dario Sosa <xdsb.tstx@gmail.com>
2024-12-17 14:45:18 -0600

    filtrado con id

--- src/controllers/product_controller.js
+++ src/controllers/product_controller.js
@@ -4,22 +4,39 @@ const createProduct = async (name, category, color, description, image, price, b
     const  newproduct  = await Product.create({ name, category,color, description, image, price, buyprice, stock, userId });
     return newproduct;
 }
-const getProductById = async (id) => {
-    const product = await Product.findByPk(id);
 
-    if(product){
-        return product;
+//funcionando en deploy
+// const getProductById = async (id) => {
+//     const product = await Product.findByPk(id);
+
+//     if(product){
+//         return product;
+//     }
+//     const userId = id;
+//     const productsByUser = await Product.findAll({where: {userId}});
+
+//     if(productsByUser.length === 0){
+//         throw new Error("Product not found")
+//     }
+
+//     return productsByUser;
+//    }
+const getProductById = async (id, userId) => {
+    const product = await Product.findOne({
+        where: {
+            id,
+            userId, // Asegúrate de que el producto pertenece al usuario
+        },
+    });
+
+    if (!product) {
+        throw new Error("Product not found");
     }
-    const userId = id;
-    const productsByUser = await Product.findAll({where: {userId}});
 
-    if(productsByUser.length === 0){
-        throw new Error("Product not found")
-    }
+    return product;
+};
 
-    return productsByUser;
-    
-}
+
 // !!!!! primer get funcionando en deploy
 // const getAllProducts = async (userId, name) =>{
 //     const products = await Product.findAll({where: {userId}});
@@ -81,18 +98,27 @@ const updateProduct = async (id, newData) => {
     return product;
 }
 
-const getProductsByCategory = async (category) => {
+
+const getProductsByCategory = async (userId, category) => {
     try {
-        const products = await Product.findAll({where: {category}});
-        if (products.length === 0){
-            return "no products found in this category";
+        // Condición para asegurar que solo se filtren los productos del usuario y la categoría indicada
+        const condition = { 
+            userId, 
+            category 
+        };
+
+        // Buscar productos con la condición
+        const products = await Product.findAll({ where: condition });
+
+        if (products.length === 0) {
+            return "No products found in this category for the logged-in user";
         }
+
         return products;
-    }catch (error) {
+    } catch (error) {
         throw new Error(error.message);
     }
-}
-
+};
 
 
 module.exports = {createProduct, getProductById, getAllProducts, deleteProduct, deleteStock, updateProduct, getProductsByCategory, };
